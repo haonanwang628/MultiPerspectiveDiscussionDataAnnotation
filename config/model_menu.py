e084eb2144020fde7b996b37206be2f445896710
haonan <143772438+haonanwang628@users.noreply.github.com>
2025-08-07 17:31:54 +0800

    Update model_menu.py

--- config/model_menu.py
+++ config/model_menu.py
@@ -7,21 +7,21 @@ MODEL_TOKENIZER_MAP = {
 llm_MaxToken = {
     "deepseek-chat": 8000,
     "deepseek-reasoner": 8000,
-    "gpt-4o-mini-2024-07-18": 10000
+    "gpt-4o-mini": 10000
 }
 
-available_models = ["deepseek-chat", "GPT-3.5", "gpt-4o-mini-2024-07-18", "Claude"]
+available_models = ["deepseek-chat", "GPT-3.5", "gpt-4o-mini", "Claude"]
 
 api_key = {
     "GPT-3.5": "sk-xxx",
-    "gpt-4o-mini-2024-07-18": "",
+    "gpt-4o-mini":"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",
     "deepseek-chat": "sk-9bc06d1289704b05b7b52db5285dba67",
     "Claude": "sk-xxx"
 }
 
 base_url = {
     "GPT-3.5": "",
-    "gpt-4o-mini-2024-07-18": "",
+    "gpt-4o-mini": "",
     "deepseek-chat": "https://api.deepseek.com/v1",
     "Claude": "https://api.anthropic.com"
 }
