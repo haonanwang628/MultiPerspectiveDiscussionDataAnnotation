a56afab511ef11b443dcce7d4b8486a024cffbc6
haonan <143772438+haonanwang628@users.noreply.github.com>
2025-08-09 22:48:33 +0800

    Update LLMs-HumanTeamDebate.py

--- streamlit/LLMs-HumanTeamDebate.py
+++ streamlit/LLMs-HumanTeamDebate.py
@@ -271,13 +271,14 @@ class MultiAgentsHumanDebate(MultiAgentsDebate):
 if __name__ == "__main__":
     debate_config = import_json("config/debate_config.json")
     models_name = {
-        "Role1": "gpt-4o-mini",
-        "Role2": "gpt-4o-mini",
-        "Human": "gpt-4o-mini",
-        "Facilitator": "gpt-4o-mini",
+        "Role1": "deepseek-chat",
+        "Role2": "deepseek-chat",
+        "Human": "deepseek-chat",
+        "Facilitator": "deepseek-chat",
     }
     app = MultiAgentsHumanDebate(debate_config, models_name)
     app.run("LLMs-HumanOutput")
+
 
 
 
